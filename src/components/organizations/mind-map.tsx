995790308d3364d03e5aacad044ca217f1cf6791
Osamu Mitsuhashi <osamu.mitsuhashi224@gmail.com>
2024-07-23 10:58:15 +0900

    ♻️重複したtype定義を削除

--- src/components/organizations/mind-map.tsx
+++ src/components/organizations/mind-map.tsx
@@ -1,21 +1,14 @@
-'use client';
+"use client";
 
-import {PropsWithoutRef, } from "react";
-import useMindMap from "@/hooks/mind-map";
-
-export type MindMapData = {
-  name: string;
-  children?: Array<MindMapData>;
-}
+import { PropsWithoutRef } from "react";
+import useMindMap, { MindMapData } from "@/hooks/mind-map";
 
 type Props = {
   data: MindMapData;
-}
-
-export default function MindMap({data}: PropsWithoutRef<Props>) {
-  const {svgRef} = useMindMap({data});
-
-  return (
-      <svg ref={svgRef}></svg>
-  );
 };
+
+export default function MindMap({ data }: PropsWithoutRef<Props>) {
+  const { svgRef } = useMindMap({ data });
+
+  return <svg ref={svgRef}></svg>;
+}
