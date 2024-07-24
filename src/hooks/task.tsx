dd9bd4470cd16324fbc8c3c8332ab6864b72f01c
Osamu Mitsuhashi <osamu.mitsuhashi224@gmail.com>
2024-07-24 11:39:40 +0900

    ✨taskを配列として扱うのはイマイチなので変更
    配列は更新コストが高すぎる

--- src/hooks/task.tsx
+++ src/hooks/task.tsx
@@ -8,53 +8,6 @@ export type TaskData = {
   children?: TaskData[];
 };
 
-export function useTasks() {
-  const [tasks, setTasks] = useState<TaskData[]>([]);
-
-  const addTask = (newTask: TaskData) => {
-    setTasks((prevTasks) => [...prevTasks, newTask]);
-  };
-
-  const addChildTask = (parentTask: TaskData) => (childTask: TaskData) => {
-    if (parentTask.children === undefined) {
-      parentTask.children = [childTask];
-    } else {
-      parentTask.children.push(childTask);
-    }
-    updateTask(parentTask);
-  };
-
-  const updateTask = (updatedTask: TaskData) => {
-    setTasks((prevTasks) =>
-      prevTasks.map((task) =>
-        task.id === updatedTask.id ? updatedTask : task,
-      ),
-    );
-  };
-
-  const deleteTask = (taskId: number) => {
-    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
-  };
-
-  const getTaskById = (taskId: number): TaskData | undefined => {
-    return tasks.find((task) => task.id === taskId);
-  };
-
-  const getChildrenTasks = (parentId: number): TaskData[] => {
-    return tasks.filter((task) => task.parent?.id === parentId);
-  };
-
-  return {
-    tasks,
-    addTask,
-    addChildTask,
-    updateTask,
-    deleteTask,
-    getTaskById,
-    getChildrenTasks,
-  };
-}
-
 export default function useTask(taskData: TaskData) {
   const [task, setTask] = useState<TaskData>(taskData);
 
